3e80138f8c1627a43e9fd14e8ba81fe8410547d1
karolinaf1293-coder <karolinaf1293@gmail.com>
2025-11-26 22:11:57 -0400

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,8 +3,9 @@
 import { createClient } from '@supabase/supabase-js';
 
 // ¡Aquí es donde pegas tus claves!
-const supabaseUrl = 'TU_URL_DE_SUPABASE'; 
-const supabaseKey = 'TU_CLAVE_ANON_PÚBLICA';
+const supabaseUrl = 'https://dkndgazqlqxyscfruroh.supabase.co'; 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrbmRnYXpxbHF4eXNjZnJ1cm9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxOTUyMzgsImV4cCI6MjA3OTc3MTIzOH0.Ju6ujS46AGpm09gBUhxEMZimLvedx8GkPft0zIMsRMA
+';
 
 // Obtienes estas claves en tu proyecto de Supabase:
 // 1. Ve a "Project Settings" (el ícono de engranaje).
